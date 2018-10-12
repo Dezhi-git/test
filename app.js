555a78e0f094044a26327a6d8a7463da19132366
Dezhi-git <17688924369@163.com>
2018-10-12 15:57:39 +0800

    testing

---
+++ app.js
@@ -0,0 +1,10 @@
+const http = require('http');
+let d = new Date();
+let s1 = d.toLocaleDateString();
+let s2 = d.toLocaleTimeString();
+http.createServer(function(req, res){
+    res.write('server last start time is '+s1+' :' + s2);
+    res.end('\n bye');
+}).listen(3000, ()=>{
+    console.log('server listening on port 3000')
+})
