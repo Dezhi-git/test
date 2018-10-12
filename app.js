ee3bc0578e472224abe6fa5e6e522f3584eecd35
Dezhi-git <17688924369@163.com>
2018-10-12 16:26:38 +0800

    add bye str

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ let s1 = d.toLocaleDateString();
 let s2 = d.toLocaleTimeString();
 http.createServer(function(req, res){
     res.write('server last start time is: '+s1+' :' + s2);
-    res.end('\n bye');
+    res.end('\n bye bye');
 }).listen(3000, ()=>{
     console.log('server listening on port 3000')
 })
