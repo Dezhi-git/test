86cd8c0683874e31e5290dd238b5d0b4f28703b5
Dezhi-git <17688924369@163.com>
2019-01-24 13:58:40 +0800

    Build-Hook: add config file

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ let d = new Date();
 let s1 = d.toLocaleDateString();
 let s2 = d.toLocaleTimeString();
 http.createServer(function(req, res){
-    res.write('Itachi:server last start time is: '+s1+' :' + s2);
+    res.write('Itachi:server last start time is: '+s1+' ------' + s2);
     res.end('\n Bye bye!');
 }).listen(3000, ()=>{
     console.log('server listening on port 3000')
